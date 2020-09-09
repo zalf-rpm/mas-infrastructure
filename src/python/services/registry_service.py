1c800629ccf3c8e65443b027f0d6fe856ed56f08
Michael Berg <michael.berg@zalf.de>
2020-09-09 13:24:06 +0200

    Update registry_service.py

--- src/python/services/registry_service.py
+++ src/python/services/registry_service.py
@@ -44,24 +44,11 @@ class Registry(reg_capnp.Service.Registry.Server):
         self._id = id if id else uuid.uuid4()
         self._name = name if name else self._uuid
         self._description = description if description else ""
-        self._services = {} # id -> {type, service}
+        self._services = {} # id -> {regToken, type, service}
 
 
     def info(self): # info @0 () -> IdInformation;
         return {"id": self._id, "name": self._name, "description": self._description}
-
-
-    def type_to_string(self, type_):
-        which = type_.which()
-        if which == "fixed":
-            type_str = str(type_.fixed)    
-            return {"str": type_str, "dict": {"fixed": type_str}}
-        elif which == "other":
-            type_str = type_.other
-            return {"str": type_str, "dict": {"other": type_str}}
-        else:
-            type_str = ""
-            return {"str": "", "dict": {"none": None}}
 
 
     def getAvailableServices_context(self, context): # getAvailableServices @0 QueryType -> (services :List(Entry));
@@ -70,55 +57,46 @@ class Registry(reg_capnp.Service.Registry.Server):
             for _, service_entry in self._services.items():
                 all_services.append(service_entry["entry"])
         else:
-            tts = self.type_to_string(context.params.type)
-            for service_entry in filter(lambda key, value: value["entry"]["type"] == tts["dict"], self._services):
+            serviceType = str(context.params.type)
+            for _, service_entry in filter(lambda key, value: value["entry"]["type"] == serviceType, self._services):
                 all_services.append(service_entry["entry"])
 
         context.results.services = all_services
 
     
-    def getService(self, id, _context, **kwargs): # getService @1 (id :Text) -> Entry;
-        if id in self._services:
-            return self._service[id]["entry"]
+    def getService_context(self, context): # getService [Service] @1 (regToken :Text) -> (service :Service);
+        regToken = context.params.regToken
+        if regToken in self._services:
+            return self._service[regToken]["entry"]["service"]
 
 
-    #async def check_id_and_store_service(self, pfp, context_results, type_str, service):
-    #    try:
-    #        info = await service.info()
-    #        self._services[info.id] = {"type": type_str, "service": service}
-    #        context_results.type = type_str
-    #        context_results.service = service
-    #    except Exception as e:
-    #        print(e)
-    #    pfp.fulfill()
-
-
-    def registerService_context(self, context): # registerService @2 (type :Type, service :Common.Identifiable) -> Entry; 
+    def registerService_context(self, context): # registerService @2 (type :Type, service :Common.Identifiable) -> (regToken :Text, unreg :Common.Registry.Unregister); 
         ps = context.params
-        tts = self.type_to_string(ps.type)
-        regId = str(uuid.uuid4())
-        unreg = Unregister(regId, self._services)
-        self._services[regId] = {"entry": {"registerId": regId, "type": tts["dict"], "service": ps.service}, "unreg": unreg}
-        context.results.unregister = unreg
-        print("registered service regId:", regId, "type:", tts["str"])
+        serviceType = ps.type
+        regToken = str(uuid.uuid4())
+        unreg = Unregister(regToken, lambda: self._services.pop(regToken, None))
+        self._services[regToken] = {"entry": {"regToken": regToken, "type": serviceType, "service": ps.service}, "unreg": unreg}
+        context.results.unreg = unreg
+        context.results.regToken = regToken
+        print("registered service regToken:", regToken, "type:", serviceType)
 
 #------------------------------------------------------------------------------
 
 class Unregister(reg_capnp.Service.Registry.Unregister.Server):
 
-    def __init__(self, id, services):
-        self._id = id
-        self._services = services
+    def __init__(self, regToken, removeServiceFunc):
+        self._regToken = regToken
+        self._removeServiceFunc = removeServiceFunc
 
 
     def __del__(self):
-        self._services.pop(self._id, None)
-        print("__del__ id:", self._id)
+        self._removeServiceFunc()
+        print("__del__ id:", self._regToken)
 
 
     def unregister(self, **kwargs): # unregister @0 ();
-        self._services.pop(self._id, None)
-        print("unregister id:", self._id)
+        self._removeServiceFunc()
+        print("unregister id:", self._regToken)
 
 #------------------------------------------------------------------------------
 
