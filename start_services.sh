d39a38e383bf338d7cf8c3f7a1a18d1d96b4fa23
Michael Berg <michael.berg@zalf.de>
2020-09-25 09:56:09 +0200

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,46 +1,49 @@
 #!/usr/bin/sh
 
-cd src/python
+#cd src/python
 
 #export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
+REG_PORT=$1
+if [ -z $1 ]; then REG_PORT=10001; fi
+
 # start registry service
-#~/.conda/envs/py38/bin/python services/registry_service.py async port=10001 &
+~/.conda/envs/py38/bin/python services/registry_service.py async port=$REG_PORT &
 
 # start BUEK1000 soil service
-~/.conda/envs/py38/bin/python services/soil/sqlite_soil_data_service.py async_register \
-reg_port=10001 \
-path_to_sqlite_db=../../data/soil/buek1000.sqlite \
-path_to_ascii_soil_grid=../../data/soil/buek1000_1000_gk5.asc \
+~/.conda/envs/py38/bin/python src/python/services/soil/sqlite_soil_data_service.py async_register \
+reg_port=$REG_PORT \
+path_to_sqlite_db=data/soil/buek1000.sqlite \
+path_to_ascii_soil_grid=data/soil/buek1000_1000_gk5.asc \
 grid_crs=gk5 \
 id=buek1000 \
 name=BUEK1000 &
 
 # start BUEK200 soil service
-~/.conda/envs/py38/bin/python services/soil/sqlite_soil_data_service.py async_register \
-reg_port=10001 \
-path_to_sqlite_db=../../data/soil/buek200.sqlite \
-path_to_ascii_soil_grid=../../data/soil/buek200_1000_gk5.asc \
+~/.conda/envs/py38/bin/python src/python/services/soil/sqlite_soil_data_service.py async_register \
+reg_port=$REG_PORT \
+path_to_sqlite_db=data/soil/buek200.sqlite \
+path_to_ascii_soil_grid=data/soil/buek200_1000_gk5.asc \
 grid_crs=gk5 \
 id=buek200 \
 name=BUEK200 &
 
-cd ../..
+#cd ../..
 
 # start some climate services
 # start DWD climate service
 ~/.conda/envs/py38/bin/python src/python/services/climate/dwd_germany_service.py async_register \
-reg_port=10001 &
+reg_port=$REG_PORT &
 
 # start DWD Bonn climate service
 ~/.conda/envs/py38/bin/python src/python/services/climate/dwd_germany_university_bonn_service.py async_register \
-reg_port=10001 &
+reg_port=$REG_PORT &
 
 # start DWD cmip cordex reklies climate service
 ~/.conda/envs/py38/bin/python src/python/services/climate/dwd_cmip_cordex_reklies_service.py async_register \
-reg_port=10001 &
+reg_port=$REG_PORT &
 
 # start Isimip global climate service
 ~/.conda/envs/py38/bin/python src/python/services/climate/isimip_service.py async_register \
-reg_port=10001 &
+reg_port=$REG_PORT &
 
