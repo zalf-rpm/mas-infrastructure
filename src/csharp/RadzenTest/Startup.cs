10ca1ab3e63a376b9f6944536721e9ea4ac879f2
Michael Berg <michael.berg@zalf.de>
2021-02-11 20:17:25 +0100

    dev dump

--- src/csharp/RadzenTest/Startup.cs
+++ src/csharp/RadzenTest/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RadzenTest.Data;
+using MudBlazor.Services;
 
 namespace RadzenTest
 {
@@ -27,6 +28,7 @@ namespace RadzenTest
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddMudServices();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<Mas.Infrastructure.Common.ConnectionManager>();
         }
