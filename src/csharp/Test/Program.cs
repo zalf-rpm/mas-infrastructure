dd0f57b22b66403448c5100c02962a2e221845d2
Michael Berg-Mohnicke <michael.berg-mohnicke@zalf.de>
2021-02-25 11:04:55 +0100

    dev dump

---
+++ src/csharp/Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Mas.Rpc;
+using System.Threading.Tasks;
+
+namespace Mas.Infrastructure.Common
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var conMan = new ConnectionManager();
+            var ts = await conMan.Connect<Climate.ITimeSeries>("capnp://localhost:11002");
+
+            var hs = await ts.Header();
+            foreach (var h in hs) Console.WriteLine(h.ToString());
+            
+        }
+    }
+}
