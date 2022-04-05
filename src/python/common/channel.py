622496030791a2e086bfa956a780656b4cec8a7c
Michael Berg-Mohnicke <michael.berg@zalf.de>
2022-04-05 19:43:18 +0200

    fix

--- src/python/common/channel.py
+++ src/python/common/channel.py
@@ -128,17 +128,15 @@ class Reader(common_capnp.Reader.Server):
         b = c._buffer
         # read value non-blocking
         if len(b) > 0:
-            #context.results.value = common_capnp.X.from_bytes(b.popleft())
             context.results.value = c.deserialize(b.popleft())
-            print(c.name, "r ", sep="", end="")
+            #print(c.name, "r ", sep="", end="")
             # unblock potentially waiting writers
             while len(b) < c._buffer_size and len(c._blocking_write_fulfillers) > 0:
                 c._blocking_write_fulfillers.popleft().fulfill()
         else: # block because no value to read
             paf = capnp.PromiseFulfillerPair()
             c._blocking_read_fulfillers.append(paf) 
-            print("[", c.name, "r"+str(len(c._blocking_read_fulfillers))+"] ", sep="", end="")
-            #return paf.promise.then(lambda: setattr(context.results, "value", common_capnp.X.from_bytes(b.popleft())))
+            #print("[", c.name, "r"+str(len(c._blocking_read_fulfillers))+"] ", sep="", end="")
             return paf.promise.then(lambda: setattr(context.results, "value", c.deserialize(b.popleft())))
 
 
@@ -153,17 +151,15 @@ class Writer(common_capnp.Writer.Server):
         b = c._buffer
         # write value non-block
         if len(b) < c._buffer_size:
-            #b.append(v.as_struct(common_capnp.X).as_builder().to_bytes())
             b.append(c.serialize(v))
-            print(c.name, "w ", sep="", end="")
+            #print(c.name, "w ", sep="", end="")
             # unblock potentially waiting readers
             while len(b) > 0 and len(c._blocking_read_fulfillers) > 0:
                 c._blocking_read_fulfillers.popleft().fulfill()
         else: # block until buffer has space
             paf = capnp.PromiseFulfillerPair()
             c._blocking_write_fulfillers.append(paf)
-            print("[", c.name, "w"+str(len(c._blocking_write_fulfillers))+"] ", sep="", end="")
-            #return paf.promise.then(lambda: b.append(v.as_struct(common_capnp.X).as_builder().to_bytes()))
+            #print("[", c.name, "w"+str(len(c._blocking_write_fulfillers))+"] ", sep="", end="")
             return paf.promise.then(lambda: b.append(c.serialize(v)))
 
 #------------------------------------------------------------------------------
@@ -191,7 +187,7 @@ async def main(no_of_channels = 1, buffer_size=1, serve_bootstrap=True, host=Non
         for arg in sys.argv[1:]:
             k, v = arg.split("=")
             if k in config:
-                config[k] = bool(v) if v.lower() in ["true", "false"] else v 
+                config[k] = v.lower() == "true" if v.lower() in ["true", "false"] else v 
     print(config)
 
     channel_to_type = {i : "Text" for i in range(1, int(config["no_of_channels"])+1)}
@@ -206,7 +202,7 @@ async def main(no_of_channels = 1, buffer_size=1, serve_bootstrap=True, host=Non
     restorer = common.Restorer()
     services = {}
     name_to_service_srs = {}
-    for i in range(1, int(no_of_channels)+1):
+    for i in range(1, int(config["no_of_channels"])+1):
         channel_value_type = channel_to_type.get(i, "Text")
         c = Channel(channel_value_type=channel_value_type, buffer_size=int(config["buffer_size"]), 
             id=config["id"], name=str(i), description=config["description"], restorer=restorer)
@@ -234,7 +230,7 @@ async def main(no_of_channels = 1, buffer_size=1, serve_bootstrap=True, host=Non
 #------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    asyncio.run(main(no_of_channels=2, buffer_size=2, serve_bootstrap=True, use_async=True)) 
+    asyncio.run(main(no_of_channels=1, buffer_size=1, serve_bootstrap=True, use_async=False)) 
 
 
 
