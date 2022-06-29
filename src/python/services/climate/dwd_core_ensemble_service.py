70a05738c64cd8afbdf2146cfd120ffb43fca7f1
Michael Berg-Mohnicke <michael.berg@zalf.de>
2022-06-29 18:27:20 +0200

    setup service from a toml config file

--- src/python/services/climate/dwd_core_ensemble_service.py
+++ src/python/services/climate/dwd_core_ensemble_service.py
@@ -21,6 +21,7 @@ import os
 from pathlib import Path
 import sys
 import time
+import tomli
 
 #remote debugging via commandline
 #-m ptvsd --host 0.0.0.0 --port 14000 --wait
@@ -36,6 +37,8 @@ if str(PATH_TO_PYTHON_CODE) not in sys.path:
 import common.capnp_async_helpers as async_helpers
 import common_climate_data_capnp_impl as ccdi
 import csv_file_based as csv_based
+import common.common as common
+import common.service as serv
 
 PATH_TO_CAPNP_SCHEMAS = PATH_TO_REPO / "capnproto_schemas"
 abs_imports = [str(PATH_TO_CAPNP_SCHEMAS)]
@@ -44,85 +47,91 @@ climate_data_capnp = capnp.load(str(PATH_TO_CAPNP_SCHEMAS / "climate_data.capnp"
 
 #------------------------------------------------------------------------------
 
-def create_meta_plus_datasets(path_to_data_dir, interpolator, rowcol_to_latlon):
+def create_meta_plus_datasets(path_to_config, config, interpolator, rowcol_to_latlon, restorer):
+
+    general = config["general"]
+    conf_datasets = config["datasets"]
+
     datasets = []
-    for gcm in os.listdir(path_to_data_dir):
-        gcm_dir = path_to_data_dir + "/" + gcm
-        if os.path.isdir(gcm_dir):
-            for rcm in os.listdir(gcm_dir):
-                rcm_dir = gcm_dir + "/" + rcm
-                if os.path.isdir(rcm_dir):
-                    for scen in os.listdir(rcm_dir):
-                        scen_dir = rcm_dir + "/" + scen
-                        if os.path.isdir(scen_dir):
-                            for ensmem in os.listdir(scen_dir):
-                                ensmem_dir = scen_dir + "/" + ensmem
-                                if os.path.isdir(ensmem_dir):
-                                    for version in os.listdir(ensmem_dir):
-                                        version_dir = ensmem_dir + "/" + version
-                                        if os.path.isdir(version_dir):
-                                            metadata = climate_data_capnp.Metadata.new_message(
-                                                entries = [
-                                                    {"gcm": ccdi.string_to_gcm(gcm)},
-                                                    {"rcm": ccdi.string_to_rcm(rcm)},
-                                                    {"historical": None} if scen == "historical" else {"rcp": scen},
-                                                    {"ensMem": ccdi.string_to_ensmem(ensmem)},
-                                                    {"version": version}
-                                                ]
-                                            )
-                                            metadata.info = ccdi.Metadata_Info(metadata)
-                                            datasets.append(climate_data_capnp.MetaPlusData.new_message(
-                                                meta=metadata, 
-                                                data=csv_based.Dataset(metadata, version_dir, interpolator, rowcol_to_latlon, 
-                                                row_col_pattern="row-{row}/col-{col}.csv")
-                                            ))
+    for ds in conf_datasets:
+        path_to_rowcols = str(path_to_config / general["path_to_rowcols"].format(gcm=ds["gcm"], rcm=ds["rcm"], scen=ds["scen"], ensmem=ds["ensmem"], version=ds["version"]))
+        metadata = climate_data_capnp.Metadata.new_message(
+            entries = [
+                {"gcm": ccdi.string_to_gcm(ds["gcm"])},
+                {"rcm": ccdi.string_to_rcm(ds["rcm"])},
+                {"historical": None} if ds["scen"] == "historical" else {"rcp": ds["scen"]},
+                {"ensMem": ccdi.string_to_ensmem(ds["ensmem"])},
+                {"version": ds["version"]},
+                {"start": ccdi.create_capnp_date(ds["start"])},
+                {"end": ccdi.create_capnp_date(ds["end"])},
+            ]
+        )
+        name = "{}_{}_{}_{}_{}".format(ds["gcm"], ds["rcm"], ds["scen"], ds["ensmem"], ds["version"])
+        metadata.info = ccdi.Metadata_Info(metadata)
+        datasets.append(climate_data_capnp.MetaPlusData.new_message(
+            meta=metadata, 
+            data=csv_based.Dataset(metadata, path_to_rowcols, interpolator, rowcol_to_latlon, 
+                row_col_pattern=general["row_col_pattern"],
+                restorer=restorer,
+                name=name
+            )
+        ))
     return datasets
 
 #------------------------------------------------------------------------------
 
-async def async_main(path_to_data, serve_bootstrap=False,
-host="0.0.0.0", port=None, reg_sturdy_ref=None, id=None, name="DWD Core Ensemble", description=None):
+async def main(path_to_config, serve_bootstrap=True, host=None, port=None, 
+    id=None, name="DWD Core Ensemble", description=None, reg_sturdy_ref=None, use_async=False):
 
     config = {
-        "path_to_data": path_to_data,
+        "path_to_config": path_to_config,
+        "config_toml_file": "metadata.toml",
+        "port": port, 
         "host": host,
-        "port": port,
         "id": id,
         "name": name,
         "description": description,
+        "serve_bootstrap": serve_bootstrap,
         "reg_sturdy_ref": reg_sturdy_ref,
-        "serve_bootstrap": str(serve_bootstrap),
         "reg_category": "climate",
+        "use_async": use_async,
     }
-    # read commandline args only if script is invoked directly from commandline
-    if len(sys.argv) > 1 and __name__ == "__main__":
-        for arg in sys.argv[1:]:
-            k, v = arg.split("=")
-            if k in config:
-                config[k] = v
-    print("config used:", config)
+    common.update_config(config, sys.argv, print_config=True, allow_new_keys=False)
 
-    conMan = async_helpers.ConnectionManager()
+    path_to_config = Path(config["path_to_config"])
+    with open(path_to_config / config["config_toml_file"], "rb") as f:
+        datasets_config = tomli.load(f)
+    
+    if not datasets_config:
+        print("Couldn't load datasets configuration from:", str(path_to_config / config["config_toml_file"]))
+        exit(1)
 
-    interpolator, rowcol_to_latlon = ccdi.create_lat_lon_interpolator_from_json_coords_file(config["path_to_data"] + "/" + "latlon-to-rowcol.json")
-    meta_plus_data = create_meta_plus_datasets(config["path_to_data"] + "/csvs", interpolator, rowcol_to_latlon)
-    service = ccdi.Service(meta_plus_data, id=config["id"], name=config["name"], description=config["description"])
+    general = datasets_config["general"]
 
+    conman = async_helpers.ConnectionManager()
+    restorer = common.Restorer()
+    interpolator, rowcol_to_latlon = ccdi.create_lat_lon_interpolator_from_json_coords_file(path_to_config / general["latlon_to_rowcol_mapping"])
+    meta_plus_data = create_meta_plus_datasets(path_to_config, datasets_config, interpolator, rowcol_to_latlon, restorer)
+    service = ccdi.Service(meta_plus_data, id=config["id"], name=config["name"], description=config["description"], restorer=restorer)
+    
     if config["reg_sturdy_ref"]:
-        registrator = await conMan.try_connect(config["reg_sturdy_ref"], cast_as=reg_capnp.Registrator)
+        registrator = await conman.try_connect(config["reg_sturdy_ref"], cast_as=reg_capnp.Registrator)
         if registrator:
             unreg = await registrator.register(ref=service, categoryId=config["reg_category"]).a_wait()
             print("Registered ", config["name"], "climate service.")
             #await unreg.unregister.unregister().a_wait()
         else:
             print("Couldn't connect to registrator at sturdy_ref:", config["reg_sturdy_ref"])
-
-    if config["serve_bootstrap"].lower() == "true":
-        await async_helpers.serve_forever(config["host"], config["port"], service)
+    
+    if config["use_async"]:
+        await serv.async_init_and_run_service({"service": service}, config["host"], config["port"], 
+        serve_bootstrap=config["serve_bootstrap"], restorer=restorer, conman=conman)
     else:
-        await conMan.manage_forever()
+        
+        serv.init_and_run_service({"service": service}, config["host"], config["port"], 
+            serve_bootstrap=config["serve_bootstrap"], restorer=restorer, conman=conman)
 
 #------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    asyncio.run(async_main("/beegfs/common/data/climate/dwd_core_ensemble", port=10009, serve_bootstrap=True))
+    asyncio.run(main("/beegfs/common/data/climate/dwd_core_ensemble", serve_bootstrap=True, use_async=True))
