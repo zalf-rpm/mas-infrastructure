5486b8c0fad02c1b9905b82ea45fde8cc3f0baaf
Michael Berg-Mohnicke <michael.berg@zalf.de>
2022-08-16 19:25:51 +0200

    update

--- src/cpp/tests/start_writers.sh
+++ src/cpp/tests/start_writers.sh
@@ -4,8 +4,7 @@ sr=$1
 to=$2
 for i in `seq $to`
 do 
-	#echo $i
-	_cmake_linux_release/test_writer -c100000 $sr/$i &
-	#_cmake_linux_debug/test_writer $sr/$i &
+	echo $i
+	#common/_cmake_release/test_writer -c100000 $sr/$i &
 done
 
