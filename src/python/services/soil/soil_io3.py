7d636bd99d6c646431d844557b4bfee47c5216fc
Michael Berg-Mohnicke <michael.berg@zalf.de>
2023-02-02 16:24:58 +0100

    fixed bug with soil conversion also in python version

--- src/python/services/soil/soil_io3.py
+++ src/python/services/soil/soil_io3.py
@@ -412,77 +412,153 @@ def sand_and_clay_to_lambda(sand, clay):
 
 #------------------------------------------------------------------------------
 
-def sand_and_clay_to_ka5_texture(sand, clay):
+def percent_sand_and_clay_to_ka5_texture(sand : int, clay : int) -> str:
     "get a rough KA5 soil texture class from given sand and soil content"
-    silt = 1.0 - sand - clay
+
     soil_texture = ""
-
-    if silt < 0.1 and clay < 0.05:
-        soil_texture = "Ss"
-    elif silt < 0.25 and clay < 0.05:
-        soil_texture = "Su2"
-    elif silt < 0.25 and clay < 0.08:
-        soil_texture = "Sl2"
-    elif silt < 0.40 and clay < 0.08:
-        soil_texture = "Su3"
-    elif silt < 0.50 and clay < 0.08:
-        soil_texture = "Su4"
-    elif silt < 0.8 and clay < 0.08:
-        soil_texture = "Us"
-    elif silt >= 0.8 and clay < 0.08:
-        soil_texture = "Uu"
-    elif silt < 0.1 and clay < 0.17:
-        soil_texture = "St2"
-    elif silt < 0.4 and clay < 0.12:
-        soil_texture = "Sl3"
-    elif silt < 0.4 and clay < 0.17:
-        soil_texture = "Sl4"
-    elif silt < 0.5 and clay < 0.17:
-        soil_texture = "Slu"
-    elif silt < 0.65 and clay < 0.17:
-        soil_texture = "Uls"
-    elif silt >= 0.65 and clay < 0.12:
-        soil_texture = "Ut2"
-    elif silt >= 0.65 and clay < 0.17:
-        soil_texture = "Ut3"
-    elif silt < 0.15 and clay < 0.25:
-        soil_texture = "St3"
-    elif silt < 0.30 and clay < 0.25:
-        soil_texture = "Ls4"
-    elif silt < 0.40 and clay < 0.25:
-        soil_texture = "Ls3"
-    elif silt < 0.50 and clay < 0.25:
-        soil_texture = "Ls2"
-    elif silt < 0.65 and clay < 0.30:
-        soil_texture = "Lu"
-    elif silt >= 0.65 and clay < 0.25:
-        soil_texture = "Ut4"
-    elif silt < 0.15 and clay < 0.35:
-        soil_texture = "Ts4"
-    elif silt < 0.30 and clay < 0.45:
-        soil_texture = "Lts"
-    elif silt < 0.50 and clay < 0.35:
-        soil_texture = "Lt2"
-    elif silt < 0.65 and clay < 0.45:
-        soil_texture = "Tu3"
-    elif silt >= 0.65 and clay >= 0.25:
-        soil_texture = "Tu4"
-    elif silt < 0.15 and clay < 0.45:
-        soil_texture = "Ts3"
-    elif silt < 0.50 and clay < 0.45:
-        soil_texture = "Lt3"
-    elif silt < 0.15 and clay < 0.65:
-        soil_texture = "Ts2"
-    elif silt < 0.30 and clay < 0.65:
-        soil_texture = "Tl"
-    elif silt >= 0.30 and clay < 0.65:
-        soil_texture = "Tu2"
-    elif clay >= 0.65:
-        soil_texture = "Tt"
-    else:
-        soil_texture = ""
-
+    silt : int = 100 - sand - clay
+    
+    if silt >= 0 and silt < 10 and clay >= 0.0 and clay < 5: 
+        soil_texture = "SS" # SS silt 0-10% clay 0-5%
+    elif silt >= 0 and silt < 10 and clay >= 5 and clay < 17:
+        soil_texture = "ST2" # ST2 silt 0-10% clay 5-17%
+    elif silt >= 0 and silt < 15 and clay >= 17 and clay < 25:
+        soil_texture = "ST3" # ST3 silt 0-15% clay 17-25%
+    elif silt >= 10 and silt < 25 and clay >= 0 and clay < 5:
+        soil_texture = "SU2" # SU2 silt 10-25% clay 0-5%
+    elif silt >= 25 and silt < 40 and clay >= 0 and clay < 8:
+        soil_texture = "SU3" # SU3 silt 25-40% clay 0-8%
+    elif silt >= 40 and silt < 50 and clay >= 0 and clay < 8:
+        soil_texture = "SU4" # SU4 silt 40-50% clay 0-8%
+    elif silt >= 10 and silt < 25 and clay >= 5 and clay < 8:
+        soil_texture = "SL2" # SL2 silt 10-25% clay 5-8%
+    elif silt >= 10 and silt < 40 and clay >= 8 and clay < 12:
+        soil_texture = "SL3" # SL3 silt 10-40% clay 8-12%
+    elif silt >= 10 and silt < 40 and clay >= 12 and clay < 17:
+        soil_texture = "SL4" # SL4 silt 10-40% clay 12-17%
+    elif silt >= 40 and silt < 50 and clay >= 8 and clay < 17:
+        soil_texture = "SLU" # SLU silt 40-50% clay 8-17%
+    elif silt >= 40 and silt < 50 and clay >= 17 and clay < 25:
+        soil_texture = "LS2" # LS2 silt 40-50% clay 17-25%
+    elif silt >= 30 and silt < 40 and clay >= 17 and clay < 25:
+        soil_texture = "LS3" # LS3 silt 30-40% clay 17-25%
+    elif silt >= 15 and silt < 30 and clay >= 17 and clay < 25:
+        soil_texture = "LS4" # LS4 silt 15-30% clay 17-25%
+    elif silt >= 30 and silt < 50 and clay >= 25 and clay < 35:
+        soil_texture = "LT2" # LT2 silt 30-50% clay 25-35%
+    elif silt >= 30 and silt < 50 and clay >= 35 and clay < 45:
+        soil_texture = "LT3" # LT3 silt 30-50% clay 35-45%
+    elif silt >= 15 and silt < 30 and clay >= 25 and clay < 45:
+        soil_texture = "LTS" # LTS silt 15-30% clay 25-45%
+    elif silt >= 50 and silt < 65 and clay >= 17 and clay < 30:
+        soil_texture = "LU" # LU silt 50-65% clay 17-30%
+    elif silt >= 50 and silt < 65 and clay >= 8 and clay < 17:
+        soil_texture = "ULS" # ULS silt 50-65% clay 8-17%
+    elif silt >= 50 and silt < 80 and clay >= 0 and clay < 8:
+        soil_texture = "US" # US silt 50-80% clay 0-8%
+    elif silt >= 80 and clay >= 0 and clay < 8:
+        soil_texture = "UU" # UU silt >80% clay 0-8%
+    elif silt >= 65 and clay >= 8 and clay < 12:
+        soil_texture = "UT2" # UT2 silt >65% clay 8-12%
+    elif silt >= 65 and clay >= 12 and clay < 17:
+        soil_texture = "UT3" # UT3 silt >65% clay 12-17%
+    elif silt >= 65 and clay >= 17 and clay < 25:
+        soil_texture = "UT4" # UT4 silt >65% clay 17-25%
+    elif silt >= 0 and silt < 15 and clay >= 45 and clay < 65:
+        soil_texture = "TS2" # TS2 silt 0-15% clay 45-65%
+    elif silt >= 0 and silt < 15 and clay >= 35 and clay < 45:
+        soil_texture = "TS3" # TS3 silt 0-15% clay 35-45%
+    elif silt >= 0 and silt < 15 and clay >= 25 and clay < 35:
+        soil_texture = "TS4" # TS4 silt 0-15% clay 25-35%
+    elif silt >= 15 and silt < 30 and clay >= 45 and clay < 65:
+        soil_texture = "TL" # TL silt 15-30% clay 45-65%
+    elif silt >= 50 and silt < 65 and clay >= 30 and clay < 45:
+        soil_texture = "TU3" # TU3 silt 50-65% clay 30-45%
+    elif silt >= 30 and clay >= 45 and clay < 65:
+        soil_texture = "TU2" # TU2 silt > 30% clay 45-65%
+    elif silt >= 65 and clay >= 25:
+        soil_texture = "TU4" # TU4 silt > 65% clay >25%
+    elif clay >= 65:
+        soil_texture = "TT" # TT clay > 65
     return soil_texture
+
+
+def sand_and_clay_to_ka5_texture(sand : float, clay : float) -> str:
+  return percent_sand_and_clay_to_ka5_texture(int(sand * 100.0), int(clay * 100.0))
+
+
+
+# def sand_and_clay_to_ka5_texture(sand, clay):
+#     "get a rough KA5 soil texture class from given sand and soil content"
+#     silt = 1.0 - sand - clay
+#     soil_texture = ""
+
+#     if silt < 0.1 and clay < 0.05:
+#         soil_texture = "Ss"
+#     elif silt < 0.25 and clay < 0.05:
+#         soil_texture = "Su2"
+#     elif silt < 0.25 and clay < 0.08:
+#         soil_texture = "Sl2"
+#     elif silt < 0.40 and clay < 0.08:
+#         soil_texture = "Su3"
+#     elif silt < 0.50 and clay < 0.08:
+#         soil_texture = "Su4"
+#     elif silt < 0.8 and clay < 0.08:
+#         soil_texture = "Us"
+#     elif silt >= 0.8 and clay < 0.08:
+#         soil_texture = "Uu"
+#     elif silt < 0.1 and clay < 0.17:
+#         soil_texture = "St2"
+#     elif silt < 0.4 and clay < 0.12:
+#         soil_texture = "Sl3"
+#     elif silt < 0.4 and clay < 0.17:
+#         soil_texture = "Sl4"
+#     elif silt < 0.5 and clay < 0.17:
+#         soil_texture = "Slu"
+#     elif silt < 0.65 and clay < 0.17:
+#         soil_texture = "Uls"
+#     elif silt >= 0.65 and clay < 0.12:
+#         soil_texture = "Ut2"
+#     elif silt >= 0.65 and clay < 0.17:
+#         soil_texture = "Ut3"
+#     elif silt < 0.15 and clay < 0.25:
+#         soil_texture = "St3"
+#     elif silt < 0.30 and clay < 0.25:
+#         soil_texture = "Ls4"
+#     elif silt < 0.40 and clay < 0.25:
+#         soil_texture = "Ls3"
+#     elif silt < 0.50 and clay < 0.25:
+#         soil_texture = "Ls2"
+#     elif silt < 0.65 and clay < 0.30:
+#         soil_texture = "Lu"
+#     elif silt >= 0.65 and clay < 0.25:
+#         soil_texture = "Ut4"
+#     elif silt < 0.15 and clay < 0.35:
+#         soil_texture = "Ts4"
+#     elif silt < 0.30 and clay < 0.45:
+#         soil_texture = "Lts"
+#     elif silt < 0.50 and clay < 0.35:
+#         soil_texture = "Lt2"
+#     elif silt < 0.65 and clay < 0.45:
+#         soil_texture = "Tu3"
+#     elif silt >= 0.65 and clay >= 0.25:
+#         soil_texture = "Tu4"
+#     elif silt < 0.15 and clay < 0.45:
+#         soil_texture = "Ts3"
+#     elif silt < 0.50 and clay < 0.45:
+#         soil_texture = "Lt3"
+#     elif silt < 0.15 and clay < 0.65:
+#         soil_texture = "Ts2"
+#     elif silt < 0.30 and clay < 0.65:
+#         soil_texture = "Tl"
+#     elif silt >= 0.30 and clay < 0.65:
+#         soil_texture = "Tu2"
+#     elif clay >= 0.65:
+#         soil_texture = "Tt"
+#     else:
+#         soil_texture = ""
+
+#     return soil_texture
 
 #------------------------------------------------------------------------------
 
@@ -501,106 +577,215 @@ def ka5_texture_to_silt(soil_type):
     return ka5_texture_to_sand_clay_silt(soil_type)["clay"]
 
 
-def ka5_texture_to_sand_clay_silt(soil_type):
+def ka5_texture_to_sand_clay_silt(soil_type : str) -> dict:
     "return {sand, clay, silt} content given KA5 soil texture"
-    xxx = (0.66, 0.0)
 
-    if soil_type == "fS":
-        xxx = (0.84, 0.02)
-    elif soil_type == "fSms":
-        xxx = (0.86, 0.02)
-    elif soil_type == "fSgs":
-        xxx = (0.88, 0.02)
-    elif soil_type == "gS":
-        xxx = (0.93, 0.02)
-    elif soil_type == "mSgs":
-        xxx = (0.96, 0.02)
-    elif soil_type == "mSfs":
-        xxx = (0.93, 0.02)
-    elif soil_type == "mS":
-        xxx = (0.96, 0.02)
-    elif soil_type == "Ss":
-        xxx = (0.93, 0.02)
-    elif soil_type == "Sl2":
-        xxx = (0.76, 0.06)
-    elif soil_type == "Sl3":
-        xxx = (0.65, 0.10)
-    elif soil_type == "Sl4":
-        xxx = (0.6, 0.14)
-    elif soil_type == "Slu":
-        xxx = (0.43, 0.12)
-    elif soil_type == "St2":
-        xxx = (0.84, 0.11)
-    elif soil_type == "St3":
-        xxx = (0.71, 0.21)
-    elif soil_type == "Su2":
-        xxx = (0.80, 0.02)
-    elif soil_type == "Su3":
-        xxx = (0.63, 0.04)
-    elif soil_type == "Su4":
-        xxx = (0.56, 0.04)
-    elif soil_type == "Ls2":
-        xxx = (0.34, 0.21)
-    elif soil_type == "Ls3":
-        xxx = (0.44, 0.21)
-    elif soil_type == "Ls4":
-        xxx = (0.56, 0.21)
-    elif soil_type == "Lt2":
-        xxx = (0.30, 0.30)
-    elif soil_type == "Lt3":
-        xxx = (0.20, 0.40)
-    elif soil_type == "Lts":
-        xxx = (0.42, 0.35)
-    elif soil_type == "Lu":
-        xxx = (0.19, 0.23)
-    elif soil_type == "Uu":
-        xxx = (0.10, 0.04)
-    elif soil_type == "Uls":
-        xxx = (0.30, 0.12)
-    elif soil_type == "Us":
-        xxx = (0.31, 0.04)
-    elif soil_type == "Ut2":
-        xxx = (0.13, 0.10)
-    elif soil_type == "Ut3":
-        xxx = (0.11, 0.14)
-    elif soil_type == "Ut4":
-        xxx = (0.09, 0.21)
-    elif soil_type == "Utl":
-        xxx = (0.19, 0.23)
-    elif soil_type == "Tt":
-        xxx = (0.17, 0.82)
-    elif soil_type == "Tl":
-        xxx = (0.17, 0.55)
-    elif soil_type == "Tu2":
-        xxx = (0.12, 0.55)
-    elif soil_type == "Tu3":
-        xxx = (0.10, 0.37)
-    elif soil_type == "Ts3":
-        xxx = (0.52, 0.40)
-    elif soil_type == "Ts2":
-        xxx = (0.37, 0.55)
-    elif soil_type == "Ts4":
-        xxx = (0.62, 0.30)
-    elif soil_type == "Tu4":
-        xxx = (0.05, 0.30)
+    soil_type = soil_type.upper()
+    x = (0.66, 0.0)
+    err = None
+
+    if soil_type == "FS": 
+        x = (0.84, 0.02)
+    elif soil_type == "FSMS":
+        x = (0.86, 0.02)
+    elif soil_type == "FSGS":
+        x = (0.88, 0.02)
+    elif soil_type == "GS":
+        x = (0.93, 0.02)
+    elif soil_type == "MSGS":
+        x = (0.96, 0.02)
+    elif soil_type == "MSFS":
+        x = (0.93, 0.02)
+    elif soil_type == "MS":
+        x = (0.96, 0.02)
+    elif soil_type == "SS":
+        x = (0.93, 0.02)
+    elif soil_type == "SL2":
+        x = (0.76, 0.06)
+    elif soil_type == "SL3":
+        x = (0.65, 0.10)
+    elif soil_type == "SL4":
+        x = (0.60, 0.14)
+    elif soil_type == "SLU":
+        x = (0.43, 0.12)
+    elif soil_type == "ST2":
+        x = (0.84, 0.11)
+    elif soil_type == "ST3":
+        x = (0.71, 0.21)
+    elif soil_type == "SU2":
+        x = (0.80, 0.02)
+    elif soil_type == "SU3":
+        x = (0.63, 0.04)
+    elif soil_type == "SU4":
+        x = (0.56, 0.04)
+    elif soil_type == "LS2":
+        x = (0.34, 0.21)
+    elif soil_type == "LS3":
+        x = (0.44, 0.21)
+    elif soil_type == "LS4":
+        x = (0.56, 0.21)
+    elif soil_type == "LT2":
+        x = (0.30, 0.30)
+    elif soil_type == "LT3":
+        x = (0.20, 0.40)
+    elif soil_type == "LTS":
+        x = (0.42, 0.35)
+    elif soil_type == "LU":
+        x = (0.19, 0.23)
+    elif soil_type == "UU":
+        x = (0.10, 0.04)
+    elif soil_type == "ULS":
+        x = (0.30, 0.12)
+    elif soil_type == "US":
+        x = (0.31, 0.04)
+    elif soil_type == "UT2":
+        x = (0.13, 0.10)
+    elif soil_type == "UT3":
+        x = (0.11, 0.14)
+    elif soil_type == "UT4":
+        x = (0.09, 0.21)
+    elif soil_type == "UTL":
+        x = (0.19, 0.23)
+    elif soil_type == "TT":
+        x = (0.17, 0.82)
+    elif soil_type == "TL":
+        x = (0.17, 0.55)
+    elif soil_type == "TU2":
+        x = (0.12, 0.55)
+    elif soil_type == "TU3":
+        x = (0.10, 0.37)
+    elif soil_type == "TS3":
+        x = (0.52, 0.40)
+    elif soil_type == "TS2":
+        x = (0.37, 0.55)
+    elif soil_type == "TS4":
+        x = (0.62, 0.30)
+    elif soil_type == "TU4":
+        x = (0.05, 0.30)
     elif soil_type == "L":
-        xxx = (0.35, 0.31)
+        x = (0.35, 0.31)
     elif soil_type == "S":
-        xxx = (0.93, 0.02)
+        x = (0.93, 0.02)
     elif soil_type == "U":
-        xxx = (0.10, 0.04)
+        x = (0.10, 0.04)
     elif soil_type == "T":
-        xxx = (0.17, 0.82)
+        x = (0.17, 0.82)
     elif soil_type == "HZ1":
-        xxx = (0.30, 0.15)
+        x = (0.30, 0.15)
     elif soil_type == "HZ2":
-        xxx = (0.30, 0.15)
+        x = (0.30, 0.15)
     elif soil_type == "HZ3":
-        xxx = (0.30, 0.15)
-    elif soil_type == "Hh":
-        xxx = (0.15, 0.1)
-    elif soil_type == "Hn":
-        xxx = (0.15, 0.1)
+        x = (0.30, 0.15)
+    elif soil_type == "HH":
+        x = (0.15, 0.1)
+    elif soil_type == "HN":
+        x = (0.15, 0.1)
+    else:
+        err = "Unknown soil type: " + soil_type + "!"
 
-    return {"sand": xxx[0], "clay": xxx[1], "silt": 1 - xxx[0] - xxx[1]}
+    return {"sand": x[0], "clay": x[1], "silt": 1 - x[0] - x[1], "err": err} 
+
+
+# def ka5_texture_to_sand_clay_silt(soil_type):
+#     "return {sand, clay, silt} content given KA5 soil texture"
+#     xxx = (0.66, 0.0)
+
+#     if soil_type == "fS":
+#         xxx = (0.84, 0.02)
+#     elif soil_type == "fSms":
+#         xxx = (0.86, 0.02)
+#     elif soil_type == "fSgs":
+#         xxx = (0.88, 0.02)
+#     elif soil_type == "gS":
+#         xxx = (0.93, 0.02)
+#     elif soil_type == "mSgs":
+#         xxx = (0.96, 0.02)
+#     elif soil_type == "mSfs":
+#         xxx = (0.93, 0.02)
+#     elif soil_type == "mS":
+#         xxx = (0.96, 0.02)
+#     elif soil_type == "Ss":
+#         xxx = (0.93, 0.02)
+#     elif soil_type == "Sl2":
+#         xxx = (0.76, 0.06)
+#     elif soil_type == "Sl3":
+#         xxx = (0.65, 0.10)
+#     elif soil_type == "Sl4":
+#         xxx = (0.6, 0.14)
+#     elif soil_type == "Slu":
+#         xxx = (0.43, 0.12)
+#     elif soil_type == "St2":
+#         xxx = (0.84, 0.11)
+#     elif soil_type == "St3":
+#         xxx = (0.71, 0.21)
+#     elif soil_type == "Su2":
+#         xxx = (0.80, 0.02)
+#     elif soil_type == "Su3":
+#         xxx = (0.63, 0.04)
+#     elif soil_type == "Su4":
+#         xxx = (0.56, 0.04)
+#     elif soil_type == "Ls2":
+#         xxx = (0.34, 0.21)
+#     elif soil_type == "Ls3":
+#         xxx = (0.44, 0.21)
+#     elif soil_type == "Ls4":
+#         xxx = (0.56, 0.21)
+#     elif soil_type == "Lt2":
+#         xxx = (0.30, 0.30)
+#     elif soil_type == "Lt3":
+#         xxx = (0.20, 0.40)
+#     elif soil_type == "Lts":
+#         xxx = (0.42, 0.35)
+#     elif soil_type == "Lu":
+#         xxx = (0.19, 0.23)
+#     elif soil_type == "Uu":
+#         xxx = (0.10, 0.04)
+#     elif soil_type == "Uls":
+#         xxx = (0.30, 0.12)
+#     elif soil_type == "Us":
+#         xxx = (0.31, 0.04)
+#     elif soil_type == "Ut2":
+#         xxx = (0.13, 0.10)
+#     elif soil_type == "Ut3":
+#         xxx = (0.11, 0.14)
+#     elif soil_type == "Ut4":
+#         xxx = (0.09, 0.21)
+#     elif soil_type == "Utl":
+#         xxx = (0.19, 0.23)
+#     elif soil_type == "Tt":
+#         xxx = (0.17, 0.82)
+#     elif soil_type == "Tl":
+#         xxx = (0.17, 0.55)
+#     elif soil_type == "Tu2":
+#         xxx = (0.12, 0.55)
+#     elif soil_type == "Tu3":
+#         xxx = (0.10, 0.37)
+#     elif soil_type == "Ts3":
+#         xxx = (0.52, 0.40)
+#     elif soil_type == "Ts2":
+#         xxx = (0.37, 0.55)
+#     elif soil_type == "Ts4":
+#         xxx = (0.62, 0.30)
+#     elif soil_type == "Tu4":
+#         xxx = (0.05, 0.30)
+#     elif soil_type == "L":
+#         xxx = (0.35, 0.31)
+#     elif soil_type == "S":
+#         xxx = (0.93, 0.02)
+#     elif soil_type == "U":
+#         xxx = (0.10, 0.04)
+#     elif soil_type == "T":
+#         xxx = (0.17, 0.82)
+#     elif soil_type == "HZ1":
+#         xxx = (0.30, 0.15)
+#     elif soil_type == "HZ2":
+#         xxx = (0.30, 0.15)
+#     elif soil_type == "HZ3":
+#         xxx = (0.30, 0.15)
+#     elif soil_type == "Hh":
+#         xxx = (0.15, 0.1)
+#     elif soil_type == "Hn":
+#         xxx = (0.15, 0.1)
+
+#     return {"sand": xxx[0], "clay": xxx[1], "silt": 1 - xxx[0] - xxx[1]}
 
