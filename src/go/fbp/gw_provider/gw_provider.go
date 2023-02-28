39193b9924bdd7d24ef6771df0a59e81fe3754c4
sanny <38938782+sannyschulz@users.noreply.github.com>
2023-02-28 19:12:34 +0100

    fixed some startup/chanel close issues

--- src/go/fbp/gw_provider/gw_provider.go
+++ src/go/fbp/gw_provider/gw_provider.go
@@ -10,6 +10,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/zalf-rpm/mas-infrastructure/capnproto_schemas/gen/go/common"
@@ -19,9 +20,9 @@ import (
 const pathToChannelExe = "../../../cpp/common/_cmake_debug/Debug/channel.exe"
 
 // const pathToChannelExe = "../../../go/fbp/channel/channel.exe"
-const pathToMasDefault = "../../../"
-const inDatasetSrDefault = "capnp://p1muS1-Eqy2eA1ijBMFDA_ouQ0-gwRJmvVZS6QYB52k=@192.168.56.1:55834/MmExYjgxYjYtMWM3YS00Y2MyLWFjNTMtNzA0MzIwNGRlZThj"
-const pathToOutDirDefault = "../../../src/python/fbp/out/"
+const pathToMasDefault = "../../../../"
+const inDatasetSrDefault = "capnp://ipFwgkCH_o-8yWOkJb2BCGCPvfGS_NsSC7tY4QYLrZ8=@192.168.56.1:51846/MTViOTE4M2UtOTFmYy00NDZiLWIxYjYtYmEyM2E1MjA2Yzk0"
+const pathToOutDirDefault = "./out/"
 
 // fbp flow groundwater provider
 func main() {
@@ -31,33 +32,13 @@ func main() {
 	pathToOutDir := flag.String("path_to_out_dir", pathToOutDirDefault, "path to output directory")
 	flag.Parse()
 
-	// start fbp flow channels
-	firstReaderSr := make(chan string)
-	firstWriterSr := make(chan string)
-	go setupFlowChannels(*pathToChannel, firstReaderSr, firstWriterSr)
-
-	// wait for first reader and writer
-	doneFR, doneFW := false, false
-	first_reader_sr, first_writer_sr := "", ""
-	for !(doneFR && doneFW) {
-		select {
-		case first_reader_sr = <-firstReaderSr:
-			doneFR = true
-		case first_writer_sr = <-firstWriterSr:
-			doneFW = true
+	// create output directory
+	if _, err := os.Stat(*pathToOutDir); os.IsNotExist(err) {
+		err = os.MkdirAll(*pathToOutDir, 0755)
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
-	fmt.Println("first reader: ", first_reader_sr)
-	fmt.Println("first writer: ", first_writer_sr)
-
-	conManager := NewConnectionManager()
-	go conManager.Run()
-
-	first_reader, err := conManager.TryConnect(first_reader_sr, 5, 5, true)
-	if err != nil {
-		log.Fatal(err)
-	}
-	firstReader := common.Channel_Reader(*first_reader)
 
 	type flow struct {
 		module1   string
@@ -73,11 +54,64 @@ func main() {
 		{"aggregate_timeseries_data_monthly", "out_sr_globrad", "write_file_3", "in_sr"},
 	}
 
+	// start fbp flow channels
+	firstReaderSr := make(chan string)
+	firstWriterSr := make(chan string)
+	channels := make([]chan bool, 0, len(flowList))
+	firstKill := make(chan bool)
+	channels = append(channels, firstKill)
+	go setupFlowChannels(*pathToChannel, firstReaderSr, firstWriterSr, firstKill)
+
+	// wait for first reader and writer
+	doneFR, doneFW := false, false
+	first_reader_sr, first_writer_sr := "", ""
+	for !(doneFR && doneFW) {
+		select {
+		case first_reader_sr = <-firstReaderSr:
+			doneFR = true
+		case first_writer_sr = <-firstWriterSr:
+			doneFW = true
+		}
+	}
+	fmt.Println("first reader: ", first_reader_sr)
+	fmt.Println("first writer: ", first_writer_sr)
+
+	// // convert to sturdy ref and back
+	// first_reader_srSR, _ := NewSturdyRefByString(first_reader_sr)
+	// // reset host to localhost
+	// first_reader_srSR.vat.address.host = "127.0.0.1"
+	// first_reader_sr = first_reader_srSR.String()
+	// firstWriterSrSR, _ := NewSturdyRefByString(first_writer_sr)
+	// firstWriterSrSR.vat.address.host = "127.0.0.1"
+	// first_writer_sr = firstWriterSrSR.String()
+
+	conManager := NewConnectionManager()
+	go conManager.Run()
+
+	first_reader, err := conManager.TryConnect(first_reader_sr, 5, 5, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	firstReader := common.Channel_Reader(*first_reader)
+
+	// verify that all components exist
+	getComponentPath := func(module string) string {
+		executable := fmt.Sprintf("%s/%s", *pathToMas, module)
+
+		executable, err := filepath.Abs(executable)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := os.Stat(executable); os.IsNotExist(err) {
+			log.Fatal(err)
+		}
+		return executable
+	}
 	create_components := map[string]func([]string) *exec.Cmd{
 		"get_climate_locations": func(srs []string) *exec.Cmd {
 			args := []string{
 				"python",
-				fmt.Sprintf("%s/src/python/fbp/get_climate_locations.py", *pathToMas),
+				getComponentPath("src/python/fbp/get_climate_locations.py"),
 				fmt.Sprintf("dataset_sr=%s", *inDatasetSr),
 				"continue_after_location_id=r:361/c:142",
 				"no_of_locations_at_once=10000",
@@ -98,7 +132,8 @@ func main() {
 			// 	args = append(args, "--"+sr)
 			// }
 			args := []string{
-				fmt.Sprintf("%s/src/python/fbp/timeseries_to_data.py", *pathToMas),
+				"python",
+				getComponentPath("src/python/fbp/timeseries_to_data.py"),
 				"in_type=capability",
 				"subrange_start=2000-01-01",
 				"subrange_end=2019-12-31",
@@ -113,7 +148,7 @@ func main() {
 		"aggregate_timeseries_data_monthly": func(srs []string) *exec.Cmd {
 			args := []string{
 				"python",
-				fmt.Sprintf("%s/src/python/fbp/aggregate_timeseries_data_monthly.py", *pathToMas),
+				getComponentPath("src/python/fbp/aggregate_timeseries_data_monthly.py"),
 			}
 			args = append(args, srs...)
 			cmd := exec.Command(args[0], args[1:]...)
@@ -122,7 +157,7 @@ func main() {
 		"write_file_1": func(srs []string) *exec.Cmd {
 			args := []string{
 				"python",
-				fmt.Sprintf("%s/src/python/fbp/write_file.py", *pathToMas),
+				getComponentPath("src/python/fbp/write_file.py"),
 				"append=true",
 				fmt.Sprintf("path_to_out_dir=%s", *pathToOutDir),
 			}
@@ -131,9 +166,10 @@ func main() {
 			return cmd
 		},
 		"write_file_2": func(srs []string) *exec.Cmd {
+
 			args := []string{
 				"python",
-				fmt.Sprintf("%s/src/python/fbp/write_file.py", *pathToMas),
+				getComponentPath("/src/python/fbp/write_file.py"),
 				"append=true",
 				fmt.Sprintf("path_to_out_dir=%s", *pathToOutDir),
 			}
@@ -144,7 +180,7 @@ func main() {
 		"write_file_3": func(srs []string) *exec.Cmd {
 			args := []string{
 				"python",
-				fmt.Sprintf("%s/src/python/fbp/write_file.py", *pathToMas),
+				getComponentPath("src/python/fbp/write_file.py"),
 				"append=true",
 				fmt.Sprintf("path_to_out_dir=%s", *pathToOutDir),
 			}
@@ -156,22 +192,28 @@ func main() {
 	compNameToComponent := map[string]map[string]string{}
 	chanIdToInOutSrNames := map[string]map[string]string{}
 
-	channels := []*exec.Cmd{}
-
 	// start all channels for the flow
 	for _, step := range flowList {
 		chan_id := step.module1 + "->" + step.module2
 		// start channel
-		newchan := startChannel(*pathToChannel, chan_id+"|"+first_writer_sr, chan_id)
-		channels = append(channels, newchan)
+		newChan := make(chan bool)
+		channels = append(channels, newChan)
+		go startChannel(*pathToChannel, chan_id+"|"+first_writer_sr, chan_id, newChan)
 
+		time.Sleep(2 * time.Second)
 		chanIdToInOutSrNames[chan_id] = map[string]string{"out": step.srNameOut, "in": step.srNameIn}
-		compNameToComponent[step.module1][step.srNameOut] = ""
-		compNameToComponent[step.module2][step.srNameIn] = ""
+
+		compNameToComponent[step.module1] = map[string]string{step.srNameOut: ""}
+
+		compNameToComponent[step.module2] = map[string]string{step.srNameIn: ""}
 	}
 
+	// sleep for a while to let channels start
+	time.Sleep(1 * time.Second)
+
 	// collect channel sturdy refs in order to start components
-	components := map[string]*exec.Cmd{}
+	componentsStarted := 0
+	componentDone := make(chan bool)
 	for {
 		// read from channels pairs of (component tuples, StartupInfo)
 		fut, rel := firstReader.Read(context.Background(), func(c common.Channel_Reader_read_Params) error {
@@ -214,13 +256,19 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-			iWSRsstr, _ := iWSRs.At(0)
+			iWSRsstr, err := iWSRs.At(0)
+			if err != nil || iWSRsstr == "" {
+				log.Fatal("iWSRsstr is empty")
+			}
 			compNameToComponent[start_comp_name][start_sr_name] = fmt.Sprintf("%s=%s", start_sr_name, iWSRsstr)
 			iRSRs, err := info.ReaderSRs()
 			if err != nil {
 				log.Fatal(err)
 			}
-			iRSRsstr, _ := iRSRs.At(0)
+			iRSRsstr, err := iRSRs.At(0)
+			if err != nil || iRSRsstr == "" {
+				log.Fatal("iRSRsstr is empty")
+			}
 			compNameToComponent[end_comp_name][end_sr_name] = fmt.Sprintf("%s=%s", end_sr_name, iRSRsstr)
 
 			// sturdy refs for all ports of start component are available
@@ -238,20 +286,16 @@ func main() {
 			}
 
 			if srs, notEmtpy := checkComponents(start_comp_name); notEmtpy {
-				components[start_comp_name] = create_components[start_comp_name](srs)
-				components[start_comp_name].Start()
-			} else {
-				log.Fatal("Flow Error: not all sturdy refs for component", start_comp_name, "are available")
+				go startComponent(start_comp_name, create_components[start_comp_name](srs), componentDone)
+				componentsStarted++
 			}
 
 			if srs, notEmtpy := checkComponents(end_comp_name); notEmtpy {
-				components[end_comp_name] = create_components[end_comp_name](srs)
-				components[end_comp_name].Start()
-			} else {
-				log.Fatal("Flow Error: not all sturdy refs for component", end_comp_name, "are available")
+				go startComponent(end_comp_name, create_components[end_comp_name](srs), componentDone)
+				componentsStarted++
 			}
 
-			if len(components) == len(compNameToComponent) {
+			if componentsStarted == len(compNameToComponent) {
 				break
 			}
 		} else {
@@ -260,20 +304,56 @@ func main() {
 
 	}
 	// wait for all components to finish
-	for _, component := range components {
-		component.Wait()
+	numComponentsFinished := 0
+	for numComponentsFinished < componentsStarted {
+		<-componentDone
+		numComponentsFinished++
 	}
 	fmt.Println("Flow Info: all components finished")
 
 	// terminate all channels
-	for _, channel := range channels {
-		channel.Process.Kill()
+	for _, killChannel := range channels {
+		killChannel <- true
 	}
-	fmt.Println("Flow Info: all channels terminated")
 
+	fmt.Println("Flow Info: all channels terminated")
+	time.Sleep(5 * time.Second)
 }
 
-func startChannel(pathToChannel string, writer_sr string, name string) *exec.Cmd {
+func startComponent(name string, cmd *exec.Cmd, done chan bool) {
+
+	cmdErr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Printf("Failed to start channel '%v '", cmd)
+		return
+	}
+	outErrScanner := bufio.NewScanner(cmdErr)
+	outErrScanner.Split(bufio.ScanLines)
+
+	go func() {
+		// read stdout for writer and reader
+		for outErrScanner.Scan() {
+			text := outErrScanner.Text()
+			fmt.Println(name, "stderr Out: ", text)
+		}
+	}()
+
+	err = cmd.Start()
+	if err != nil {
+		log.Println(name, err)
+		//log.Fatal(name, err)
+		done <- false
+		return
+	}
+	err = cmd.Wait()
+
+	if err != nil {
+		log.Println(name, err)
+	}
+	done <- true
+}
+
+func startChannel(pathToChannel string, writer_sr string, name string, killChan <-chan bool) {
 	if name == "" {
 		name = uuid.New().String()
 	}
@@ -281,14 +361,50 @@ func startChannel(pathToChannel string, writer_sr string, name string) *exec.Cmd
 	args := []string{
 		fmt.Sprintf("--name=chan_%s", name),
 		fmt.Sprintf("--startup_info_writer_sr=%s", writer_sr),
+		"--verbose",
 	}
 
 	cmd := exec.Command(pathToChannel, args...)
-	return cmd
+
+	go func() {
+		<-killChan // wait for kill signal
+		if cmd.Process != nil {
+			err := cmd.Process.Kill()
+			if err != nil {
+				log.Println(name, err)
+			}
+		}
+	}()
+	cmdErr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Printf("Failed to start channel '%v '", cmd)
+		return
+	}
+	outErrScanner := bufio.NewScanner(cmdErr)
+	outErrScanner.Split(bufio.ScanLines)
+
+	go func() {
+		// read stdout for writer and reader
+		for outErrScanner.Scan() {
+			text := outErrScanner.Text()
+			fmt.Println(name, "stderr Out: ", text)
+		}
+	}()
+
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(name, err)
+	}
+	err = cmd.Wait()
+
+	if err != nil {
+		log.Println(name, err)
+	}
+
 }
 
 // start fbp flow channels
-func setupFlowChannels(pathToChannel string, firstReaderSr, firstWriterSr chan<- string) {
+func setupFlowChannels(pathToChannel string, firstReaderSr, firstWriterSr chan<- string, killChan <-chan bool) {
 
 	// start external process
 	args := []string{
@@ -321,7 +437,7 @@ func setupFlowChannels(pathToChannel string, firstReaderSr, firstWriterSr chan<-
 		channelNameReceived := 0
 		for outScanner.Scan() {
 			text := outScanner.Text()
-			fmt.Println("Channel Out: ", text)
+			fmt.Println("First Channel: ", text)
 			if channelNameReceived < 2 {
 				s := strings.SplitN(text, "=", 2)
 				if len(s) == 2 {
@@ -351,10 +467,18 @@ func setupFlowChannels(pathToChannel string, firstReaderSr, firstWriterSr chan<-
 		// read stdout for writer and reader
 		for outErrScanner.Scan() {
 			text := outErrScanner.Text()
-			fmt.Println("Std ERR Out: ", text)
+			fmt.Println("First strErr: ", text)
 		}
 	}()
-
+	go func() {
+		<-killChan // wait for kill signal
+		if cmd.Process != nil {
+			err := cmd.Process.Kill()
+			if err != nil {
+				log.Println("First", err)
+			}
+		}
+	}()
 	err = cmd.Start()
 	if err != nil {
 		log.Fatal(err)
