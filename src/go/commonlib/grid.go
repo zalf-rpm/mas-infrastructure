5632593c1295046d00627f0f371284b071bfe9e8
sanny <38938782+sannyschulz@users.noreply.github.com>
2023-03-08 16:15:54 +0100

    implement missing methodes

--- src/go/commonlib/grid.go
+++ src/go/commonlib/grid.go
@@ -10,6 +10,11 @@ import (
 	"github.com/zalf-rpm/mas-infrastructure/capnproto_schemas/gen/go/persistence"
 )
 
+// Grid is a struct that implements the Grid_Server interface
+// for immutable grids, the functions that need to be implemented are
+// GetValueRowCol, GetValueLatLon
+// GetValueRowColAggregated, GetValueLatLonAggregated
+// not thread safe if the grid below is changing
 type Grid struct {
 	persistable *Persistable
 	info        *Identifiable
@@ -18,11 +23,14 @@ type Grid struct {
 	NumRows              uint64
 	NumCols              uint64
 	NoDataType           interface{} // valid types are int64, float64, bool, uint64
-	bounds               LatLonBoundaries
-	boundsFromCellCenter LatLonBoundaries
+	Bounds               LatLonBoundaries
+	BoundsFromCellCenter LatLonBoundaries
 
-	GetValueRowCol func(row, col uint64) interface{}
-	GetValueLatLon func(lat, lon float64) interface{}
+	// functions that need to be implemented to use the grid capnp interface
+	GetValueRowCol           func(row, col uint64) (interface{}, error)
+	GetValueLatLon           func(lat, lon float64) (interface{}, RowCol, RowCol, error)
+	GetValueRowColAggregated func(row, col uint64, resolution uint64, agg string, includeAggParts bool) (interface{}, []AggregationPart, error)
+	GetValueLatLonAggregated func(lat, lon float64, resolution uint64, agg string, includeAggParts bool) (interface{}, []AggregationPart, error)
 }
 
 func NewGrid(saveChan chan *SaveMsg, id, name, description string) *Grid {
@@ -58,9 +66,123 @@ type LatLonBoundaries struct {
 	BottomLeft  LatLon
 	BottomRight LatLon
 }
+type RowCol struct {
+	Row uint64
+	Col uint64
+}
+type AggregationPart struct {
+	OriginalValue     interface{} // valid types are int64, float64, bool, uint64
+	RowColTuple       RowCol
+	AreaWeight        float64
+	interpolatedValue float64
+}
 
 // Grid_Server interface
-func (g *Grid) ClosestValueAt(c context.Context, call grid.Grid_closestValueAt) error { return nil }
+func (g *Grid) ClosestValueAt(c context.Context, call grid.Grid_closestValueAt) error {
+	latLon, err := call.Args().LatlonCoord()
+	if err != nil {
+		return err
+	}
+	lat := latLon.Lat()
+	lon := latLon.Lon()
+	resolution := call.Args().Resolution()
+	agg := call.Args().Agg()
+	includeAggParts := call.Args().IncludeAggParts()
+	ignoreNoData := call.Args().IgnoreNoData()
+	returnRowCols := call.Args().ReturnRowCols()
+
+	result, err := call.AllocResults()
+	if err != nil {
+		return err
+	}
+	var returnVal interface{}
+	var aggrParts []AggregationPart
+	var topLeftrowCol RowCol
+	var bottomRightRowCol RowCol
+	if resolution >= g.GridResolution {
+		// get value from grid (lat, lon)
+		if g.GetValueLatLon == nil {
+			return errors.New("Grid.GetValueLatLon is not implemented")
+		}
+		returnVal, topLeftrowCol, bottomRightRowCol, err = g.GetValueLatLon(lat, lon)
+		if err != nil {
+			return err
+		}
+	} else if resolution < g.GridResolution && agg.String() == "none" {
+		returnVal = 0
+	} else if resolution < g.GridResolution && agg.String() != "none" {
+
+		// call grid implementation of aggregation
+		if g.GetValueLatLonAggregated == nil {
+			return errors.New("Grid.GetValueLatLonAggregated is not implemented")
+		}
+		returnVal, aggrParts, err = g.GetValueLatLonAggregated(lat, lon, resolution, agg.String(), ignoreNoData)
+		if err != nil {
+			return err
+		}
+		if includeAggParts {
+			lenAggrParts := len(aggrParts)
+			aggList, err := result.NewAggParts(int32(lenAggrParts))
+			if err != nil {
+				return err
+			}
+			for i := 0; i < lenAggrParts; i++ {
+				aggPart, err := grid.NewGrid_AggregationPart(aggList.Segment())
+				if err != nil {
+					return err
+				}
+				aggPart.SetAreaFrac(aggrParts[i].AreaWeight)
+				aggPart.SetIValue(aggrParts[i].interpolatedValue)
+				origVal, err := aggPart.NewValue()
+				if err != nil {
+					return err
+				}
+				SetGridValue(origVal, aggrParts[i].OriginalValue)
+				aggPart.SetValue(origVal)
+				rowcol, err := aggPart.NewRowCol()
+				if err != nil {
+					return err
+				}
+				rowcol.SetRow(aggrParts[i].RowColTuple.Row)
+				rowcol.SetCol(aggrParts[i].RowColTuple.Col)
+				aggPart.SetRowCol(rowcol)
+				aggList.Set(i, aggPart)
+			}
+		}
+
+	}
+
+	if returnRowCols {
+		// get row and col of closest value
+		rowCol, err := result.NewBr()
+		if err != nil {
+			return err
+		}
+		rowCol.SetRow(bottomRightRowCol.Row)
+		rowCol.SetCol(bottomRightRowCol.Col)
+		result.SetBr(rowCol)
+		rowCol, err = result.NewTl()
+		if err != nil {
+			return err
+		}
+		rowCol.SetRow(topLeftrowCol.Row)
+		rowCol.SetCol(topLeftrowCol.Col)
+		result.SetTl(rowCol)
+	}
+	val, err := result.NewVal()
+	if err != nil {
+		return err
+	}
+	err = SetGridValue(val, returnVal)
+	if err != nil {
+		return err
+	}
+	err = result.SetVal(val)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 func (g *Grid) Resolution(c context.Context, call grid.Grid_resolution) error {
 	result, err := call.AllocResults()
 	if err != nil {
@@ -89,18 +211,11 @@ func (g *Grid) NoDataValue(c context.Context, call grid.Grid_noDataValue) error 
 	if err != nil {
 		return err
 	}
-	switch val := g.NoDataType.(type) {
-	case int64:
-		gridVal.SetI(val)
-	case float64:
-		gridVal.SetF(val)
-	case bool:
-		gridVal.SetNo(val)
-	case uint64:
-		gridVal.SetUi(val)
-	default:
-		return errors.New("unknown type for NoDataValue")
+	err = SetGridValue(gridVal, g.NoDataType)
+	if err != nil {
+		return err
 	}
+
 	return result.SetNodata(gridVal)
 }
 func (g *Grid) ValueAt(c context.Context, call grid.Grid_valueAt) error {
@@ -115,50 +230,88 @@ func (g *Grid) ValueAt(c context.Context, call grid.Grid_valueAt) error {
 	if err != nil {
 		return err
 	}
-
-	if agg.String() != "none" {
-
-		//TBD: implement aggregation
-		var listLen int32 = 1
-		list, err := result.NewAggParts(listLen)
-		if err != nil {
-			return err
-		}
-
-		aggPart, err := grid.NewGrid_AggregationPart(list.Segment())
-		if err != nil {
-			return err
-		}
-
-	} else {
-		gridVal, err := result.NewVal()
-		if err != nil {
-			return err
+	var returnVal interface{}
+	var aggrParts []AggregationPart
+	if resolution >= g.GridResolution {
+		if g.GetValueRowCol == nil {
+			return errors.New("Grid.GetValueRowCol is not implemented")
 		}
 		// get value from grid (row, col)
-		value := g.GetValueRowCol(row, col)
-		switch val := value.(type) {
-		case int64:
-			gridVal.SetI(val)
-		case float64:
-			gridVal.SetF(val)
-		case bool:
-			gridVal.SetNo(val)
-		case uint64:
-			gridVal.SetUi(val)
-		default:
-			return errors.New("unknown type for NoDataValue")
+		returnVal, err = g.GetValueRowCol(row, col)
+		if err != nil {
+			return err
+		}
+	} else if resolution < g.GridResolution && agg.String() == "none" {
+		returnVal = 0
+	} else if resolution < g.GridResolution && agg.String() != "none" {
+
+		if g.GetValueRowColAggregated == nil {
+			return errors.New("Grid.GetValueRowColAggregated is not implemented")
+		}
+		// call grid implementation of aggregation
+		returnVal, aggrParts, err = g.GetValueRowColAggregated(row, col, resolution, agg.String(), includeAggParts)
+		if err != nil {
+			return err
+		}
+		if includeAggParts {
+			var listLen int32 = int32(len(aggrParts))
+			list, err := result.NewAggParts(listLen)
+			if err != nil {
+				return err
+			}
+			for i, part := range aggrParts {
+				aggPart, err := grid.NewGrid_AggregationPart(list.Segment())
+				if err != nil {
+					return err
+				}
+				rowCol, err := aggPart.NewRowCol()
+				if err != nil {
+					return err
+				}
+				rowCol.SetRow(part.RowColTuple.Row)
+				rowCol.SetCol(part.RowColTuple.Col)
+				aggPart.SetRowCol(rowCol)
+
+				aggPart.SetAreaFrac(part.AreaWeight)
+				aggPart.SetIValue(part.interpolatedValue)
+				val, err := aggPart.NewValue()
+				if err != nil {
+					return err
+				}
+				SetGridValue(val, part.OriginalValue)
+				aggPart.SetValue(val)
+				list.Set(i, aggPart)
+			}
 		}
 	}
 
-	// (row :UInt64, col :UInt64,
-	// 	resolution :UInt64,
-	// 	agg :Aggregation = none,
-	// 	includeAggParts :Bool = false)
-	// 	-> (val :Value, aggParts :List(AggregationPart));
+	// set value to result
+	gridVal, err := result.NewVal()
+	if err != nil {
+		return err
+	}
+	SetGridValue(gridVal, returnVal)
+	result.SetVal(gridVal)
 
 	return nil
 }
+
+func SetGridValue(gV grid.Grid_Value, val interface{}) error {
+	switch val := val.(type) {
+	case int64:
+		gV.SetI(val)
+	case float64:
+		gV.SetF(val)
+	case bool:
+		gV.SetNo(val)
+	case uint64:
+		gV.SetUi(val)
+	default:
+		return errors.New("unknown type for value")
+	}
+	return nil
+}
+
 func (g *Grid) LatLonBounds(c context.Context, call grid.Grid_latLonBounds) error {
 	useCellcenter := call.Args().UseCellCenter()
 
@@ -172,8 +325,8 @@ func (g *Grid) LatLonBounds(c context.Context, call grid.Grid_latLonBounds) erro
 		if err != nil {
 			return err
 		}
-		bl.SetLat(g.boundsFromCellCenter.BottomLeft.Lat)
-		bl.SetLon(g.boundsFromCellCenter.BottomLeft.Lon)
+		bl.SetLat(g.BoundsFromCellCenter.BottomLeft.Lat)
+		bl.SetLon(g.BoundsFromCellCenter.BottomLeft.Lon)
 		err = result.SetBl(bl)
 		if err != nil {
 			return err
@@ -183,8 +336,8 @@ func (g *Grid) LatLonBounds(c context.Context, call grid.Grid_latLonBounds) erro
 		if err != nil {
 			return err
 		}
-		br.SetLat(g.boundsFromCellCenter.BottomRight.Lat)
-		br.SetLon(g.boundsFromCellCenter.BottomRight.Lon)
+		br.SetLat(g.BoundsFromCellCenter.BottomRight.Lat)
+		br.SetLon(g.BoundsFromCellCenter.BottomRight.Lon)
 		err = result.SetBr(br)
 		if err != nil {
 			return err
@@ -194,8 +347,8 @@ func (g *Grid) LatLonBounds(c context.Context, call grid.Grid_latLonBounds) erro
 		if err != nil {
 			return err
 		}
-		tl.SetLat(g.boundsFromCellCenter.TopLeft.Lat)
-		tl.SetLon(g.boundsFromCellCenter.TopLeft.Lon)
+		tl.SetLat(g.BoundsFromCellCenter.TopLeft.Lat)
+		tl.SetLon(g.BoundsFromCellCenter.TopLeft.Lon)
 		err = result.SetTl(tl)
 		if err != nil {
 			return err
@@ -205,8 +358,8 @@ func (g *Grid) LatLonBounds(c context.Context, call grid.Grid_latLonBounds) erro
 		if err != nil {
 			return err
 		}
-		tr.SetLat(g.boundsFromCellCenter.TopRight.Lat)
-		tr.SetLon(g.boundsFromCellCenter.TopRight.Lon)
+		tr.SetLat(g.BoundsFromCellCenter.TopRight.Lat)
+		tr.SetLon(g.BoundsFromCellCenter.TopRight.Lon)
 		err = result.SetTr(tr)
 		if err != nil {
 			return err
@@ -216,8 +369,8 @@ func (g *Grid) LatLonBounds(c context.Context, call grid.Grid_latLonBounds) erro
 		if err != nil {
 			return err
 		}
-		bl.SetLat(g.bounds.BottomLeft.Lat)
-		bl.SetLon(g.bounds.BottomLeft.Lon)
+		bl.SetLat(g.Bounds.BottomLeft.Lat)
+		bl.SetLon(g.Bounds.BottomLeft.Lon)
 		err = result.SetBl(bl)
 		if err != nil {
 			return err
@@ -226,8 +379,8 @@ func (g *Grid) LatLonBounds(c context.Context, call grid.Grid_latLonBounds) erro
 		if err != nil {
 			return err
 		}
-		br.SetLat(g.bounds.BottomRight.Lat)
-		br.SetLon(g.bounds.BottomRight.Lon)
+		br.SetLat(g.Bounds.BottomRight.Lat)
+		br.SetLon(g.Bounds.BottomRight.Lon)
 		err = result.SetBr(br)
 		if err != nil {
 			return err
@@ -237,8 +390,8 @@ func (g *Grid) LatLonBounds(c context.Context, call grid.Grid_latLonBounds) erro
 		if err != nil {
 			return err
 		}
-		tl.SetLat(g.bounds.TopLeft.Lat)
-		tl.SetLon(g.bounds.TopLeft.Lon)
+		tl.SetLat(g.Bounds.TopLeft.Lat)
+		tl.SetLon(g.Bounds.TopLeft.Lon)
 		err = result.SetTl(tl)
 		if err != nil {
 			return err
@@ -248,8 +401,8 @@ func (g *Grid) LatLonBounds(c context.Context, call grid.Grid_latLonBounds) erro
 		if err != nil {
 			return err
 		}
-		tr.SetLat(g.bounds.TopRight.Lat)
-		tr.SetLon(g.bounds.TopRight.Lon)
+		tr.SetLat(g.Bounds.TopRight.Lat)
+		tr.SetLon(g.Bounds.TopRight.Lon)
 		err = result.SetTr(tr)
 		if err != nil {
 			return err
@@ -257,7 +410,10 @@ func (g *Grid) LatLonBounds(c context.Context, call grid.Grid_latLonBounds) erro
 	}
 	return nil
 }
-func (g *Grid) StreamCells(c context.Context, call grid.Grid_streamCells) error { return nil }
+func (g *Grid) StreamCells(c context.Context, call grid.Grid_streamCells) error {
+
+	return nil
+}
 
 // Identifiable_Server interface
 func (g *Grid) Info(c context.Context, call common.Identifiable_info) error {
