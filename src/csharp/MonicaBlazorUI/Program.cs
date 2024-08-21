f59f8133b3d2092e4e6c5414f5b554af5671b78d
Michael Berg-Mohnicke <michael.berg@zalf.de>
2024-08-21 15:56:09 +0200

    ui compiles again, but still has bugs

--- src/csharp/MonicaBlazorUI/Program.cs
+++ src/csharp/MonicaBlazorUI/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using MudBlazor.Services;
 using Blazored.LocalStorage;
+using GoogleMapsComponents;
+using GoogleMapsComponents.Maps;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +14,17 @@ StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configurat
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
+//builder.Services.AddMudPopoverService();
 builder.Services.AddSingleton<Mas.Infrastructure.Common.ConnectionManager>();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddTransient<Allegiance.Blazor.Highcharts.Core.Services.Interfaces.IChartService, Allegiance.Blazor.Highcharts.Core.Services.ChartService>();
 builder.Services.AddTransient<MonicaBlazorUI.Services.MonicaIO>();
 builder.Services.AddTransient<MonicaBlazorUI.Services.RunMonica>();
-//builder.Services.AddBlazorGoogleMaps("")
+//builder.Services.AddBlazorGoogleMaps(Environment.GetEnvironmentVariable("GOOGLE_MAPS_API_KEY") ?? "");
+builder.Services.AddBlazorGoogleMaps(new MapApiLoadOptions(Environment.GetEnvironmentVariable("GOOGLE_MAPS_API_KEY") ?? "")
+{
+  Version = "3"
+});
 
 var app = builder.Build();
 
